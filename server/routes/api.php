5b5342fc6aab825cf22c7c05e68e341db9632103
Yuzunoha <studysteadystandby@gmail.com>
2020-01-20 12:18:14 +0900

    ルート追加

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 Route::get('/v2', function (Request $request) {
     return "hello";
 });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
