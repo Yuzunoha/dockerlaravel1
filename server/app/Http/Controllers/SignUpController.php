02f141cbf7123ad4fa5b81b3cc43a334da3354af
Yuzunoha <studysteadystandby@gmail.com>
2020-01-23 20:14:08 +0900

    全部何かしら入っているの判定まではできた

--- server/app/Http/Controllers/SignUpController.php
+++ server/app/Http/Controllers/SignUpController.php
@@ -11,16 +11,54 @@ class SignUpController extends Controller
      */
     public function post(Request $request)
     {
+        // ガード:name, bio, email, pass, passconf があること
+        $errMsgList = [];
+        if ($request->sign_up_user_params['name']) {
+            $name = $request->sign_up_user_params['name'];
+        } else {
+            $errMsgList[] = "nameを入力してください";
+        }
+        if ($request->sign_up_user_params['bio']) {
+            $bio = $request->sign_up_user_params['bio'];
+        } else {
+            $errMsgList[] = "bioを入力してください";
+        }
+        if ($request->sign_up_user_params['email']) {
+            $email = $request->sign_up_user_params['email'];
+        } else {
+            $errMsgList[] = "emailを入力してください";
+        }
+        if ($request->sign_up_user_params['password']) {
+            $password = $request->sign_up_user_params['password'];
+        } else {
+            $errMsgList[] = "passwordを入力してください";
+        }
+        if ($request->sign_up_user_params['password_confirmation']) {
+            $password_confirmation = $request->sign_up_user_params['password_confirmation'];
+        } else {
+            $errMsgList[] = "password_confirmationを入力してください";
+        }
+        if (0 !== count($errMsgList)) {
+            /* 空なパラメタがあった */
+            return ['error' => ['messages' => $errMsgList]];
+        }
+        return ["result" => "全部何かしら入っている"];
+
+        // ガード:emailが登録済みでないこと
+        // ガード:passとpassconfが一致すること
+
         // このレスポンスで、正しい値にすればログインできる
+        /*
         $obj = [
-            "id" => 800,
-            "name" => "a",
-            "bio" => "a",
-            "token" => "YkVQrQEHkhGrr5vvNDBicQtt",
-            "email" => "a@a1",
-            "created_at" => "2020-01-23T10:22:22.460Z",
-            "updated_at" => "2020-01-23T10:22:22.460Z",
+        "id" => 800,
+        "name" => "a",
+        "bio" => "a",
+        "token" => "YkVQrQEHkhGrr5vvNDBicQtt",
+        "email" => "a@a1",
+        "created_at" => "2020-01-23T10:22:22.460Z",
+        "updated_at" => "2020-01-23T10:22:22.460Z",
         ];
-        return $obj;
+         */
+        return ['a' => 'b'];
     }
 }
