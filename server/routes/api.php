74d2b6efe0f350f3a7a34c9c0a35ab9db50ee1c8
Yuzunoha <studysteadystandby@gmail.com>
2020-01-23 17:51:17 +0900

    ルートを整理した。/api/v2をプレフィックスとする

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::get('/v2', function (Request $request) {
-    return "hello";
-});
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/v2/user', function (Request $request) {
     return $request->user();
 });
 
