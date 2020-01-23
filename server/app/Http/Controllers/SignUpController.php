b7240a152584520850158e78387e333be0ffb4e4
Yuzunoha <studysteadystandby@gmail.com>
2020-01-23 18:49:16 +0900

    postができた

--- server/app/Http/Controllers/SignUpController.php
+++ server/app/Http/Controllers/SignUpController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class SignUpController extends Controller
 {
-    //
+    /**
+     * post sing_up
+     */
+    public function post(Request $request)
+    {
+        return [
+            'keyA' => 'valueA',
+            'keyB' => 'valueB',
+            'keyC' => 'valueC',
+        ];
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/v2/user', function (Request $request) {
+Route::middleware('auth:api')->get('v2/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/v2/sign_up', function (Request $request) {
-    return "sign_upのapiです";
-});
+Route::post('v2/sign_up', 'SignUpController@post');
